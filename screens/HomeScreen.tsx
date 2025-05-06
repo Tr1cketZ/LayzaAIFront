646e0ccb8c06ddeaa625ffefdc423a579e017987
Bruno Martins <88806986+BrunoMartinsGameDev@users.noreply.github.com>
2025-05-06 16:28:03 -0300

    Feito HomeScreen parcial
    Ira virar a tela de recuperar senha
    Tenho que refatorar a questao do navigation pois ele se repete na tela de login

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import globalStyles from '../styles/globalStyles';
+import Sofa from 'react-native-vector-icons/FontAwesome5';
+import BackArrow from '../components/BackArrow';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const HomeScreen: React.FC = () => {
+type RootStackParamList = {
+  Loading: undefined;
+  Login: undefined;
+  LoginForm: undefined; // Nova tela
+  CriarConta: undefined;
+  Home: undefined;
+  EsqueceuSenha: undefined; // Para "Esqueceu a senha"
+};
+type LoginFormNavigationProp = StackNavigationProp<RootStackParamList, 'LoginForm'>;
+
+interface Props {
+  navigation: LoginFormNavigationProp;
+}
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
   return (
+    <LinearGradient
+    colors={['#0172B2', '#001645']}
+    start={{ x: 0, y: 0 }}
+    end={{ x: 0, y: 1 }}
+    style={globalStyles.container}
+  >
     <View style={globalStyles.container}>
+      <BackArrow navigation={navigation} />
+      <Sofa name="couch" size={200} color="#2F80ED" />
       <Text style={globalStyles.title}>Bem-vindo ao Layza!</Text>
     </View>
+    </LinearGradient>
   );
 };
 
